15c5ab20b6783f09cf562e3513ed945fc8f3262a
HoangVu2312 <103991547+HoangVu2312@users.noreply.github.com>
2024-01-25 14:09:15 +0200

    Create CartLoader.js

---
+++ src/components/CartLoader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CartLoader.css'; // Assuming you have a CSS file for your styles
+import { Container } from 'react-bootstrap';
+
+const CartLoader = () => {
+  return (
+    <Container className='cart-container'>
+    <div className='stroke'>
+    </div>
+  </Container>
+  );
+};
+
+export default CartLoader;
