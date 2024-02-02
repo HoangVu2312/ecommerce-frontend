fcb817d0ef71b815f8f42c69cfeb9965097d7b48
HoangVu2312 <103991547+HoangVu2312@users.noreply.github.com>
2024-02-02 02:08:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
-import CartLoader from './components/CartLoader.js';
+
 
 //import for deploy
 import {disableReactDevTools} from "@fvilers/disable-react-devtools"
@@ -22,7 +22,7 @@ const persistedStore = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-        <PersistGate loading={<CartLoader />} persistor={persistedStore}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistedStore}>
             <App />
         </PersistGate>
     </Provider>
